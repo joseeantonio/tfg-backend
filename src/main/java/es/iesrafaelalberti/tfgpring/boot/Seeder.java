7d7c5c005edb66eaaf0f3662ba51b2c043e6b6d2
jose <jagonzalez.alumno@iesrafaelalberti.es>
2023-05-23 18:41:34 +0200

    seeder con producto

--- src/main/java/es/iesrafaelalberti/tfgpring/boot/Seeder.java
+++ src/main/java/es/iesrafaelalberti/tfgpring/boot/Seeder.java
@@ -1,5 +1,6 @@
 package es.iesrafaelalberti.tfgpring.boot;
 
+import es.iesrafaelalberti.tfgpring.models.Producto;
 import es.iesrafaelalberti.tfgpring.repositories.ClienteRepository;
 import es.iesrafaelalberti.tfgpring.repositories.PedidoRepository;
 import es.iesrafaelalberti.tfgpring.repositories.ProductoPedidoRepository;
@@ -26,6 +27,28 @@ public class Seeder implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        Producto producto1 = new Producto("Anillo de diamante",
+                "https://i.imgur.com/6mkk4WD.jpg",
+                "Anillo de diamantes, creado en oro de 18 quilates o platino, con un diamante central en talla brillante, engarzado en 6 delicadas garras de forma tradicional, para mostrar toda la luz de la gema. Es un solitario de diamantes tradicional que sin embargo presenta un brazo lleno de diseño y tendencia, con un doble perfil.",
+                100f,
+                "Anillo",
+                "Hombre",
+                "9",
+                null,
+                null,
+                null,
+                null,
+                null,
+                false,
+
+                "Dorado",
+                "14 gramos",
+                "14K",
+                "Diamante",
+                5L,
+                null,
+                null,
+                null);
 
     }
 }
