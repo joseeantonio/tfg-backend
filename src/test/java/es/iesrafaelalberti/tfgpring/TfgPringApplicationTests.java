3445660b8b2e6a538175ab8fcc7d193ddeee21f6
jose <jagonzalez.alumno@iesrafaelalberti.es>
2023-06-16 01:06:55 +0200

    crear pedido

--- src/test/java/es/iesrafaelalberti/tfgpring/TfgPringApplicationTests.java
+++ src/test/java/es/iesrafaelalberti/tfgpring/TfgPringApplicationTests.java
@@ -1,13 +1,8 @@
 package es.iesrafaelalberti.tfgpring;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TfgPringApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
